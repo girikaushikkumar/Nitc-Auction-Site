5f651b8e91fba07cafbf8c214e1cab3fb9772658
Kaushik Kumar Giri <139802296+girikaushikkumar@users.noreply.github.com>
2024-04-07 10:58:09 +0530

    Update DBConnection.java

--- src/nitc/auction/site/DBConnection.java
+++ src/nitc/auction/site/DBConnection.java
@@ -14,7 +14,7 @@ import java.sql.DriverManager;
 public class DBConnection {
     private static final String user="root";
     private static final String url="jdbc:mysql://localhost:3306/auctiondb";
-    private static final String pass="smilegiri";
+    private static final String pass="******";
    
     public static Connection connectDB()
     {
